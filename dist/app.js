3ef6537982f1e9c96cb49e4b508bee2e3400e65b
aaichele <aichelealec@gmail.com>
2019-05-30 12:01:47 -0700

    updated from nicks

--- dist/app.js
+++ dist/app.js
@@ -1,67 +1,70 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const colorService = require('./services/color-service');
+const ColorService = require('./services/color-service');
 
 $(document).ready(() => {
-    $('#searchBtn').on('click', () => {
-        let searchText = $('#search').val();
+  $('#searchBtn').on('click', () => {
+    const searchText = $('#searchText').val();
 
-        colorService
-            .searchColors(searchText)
-            .then((results) => {
-                const paletteTemplate = $('#paletteTemplate');
-                const output = $('#output');
+    ColorService
+      .searchColors(searchText)
+      .then((results) => {
+        const paletteTemplate = $('#paletteTemplate');
+        const paletteHTML = paletteTemplate.html().trim();
+        const output = $('#output');
 
-                results.forEach((palette) => {
-                    let paletteHtml = paletteTemplate.html().trim();
-                    let $palette = $(paletteHtml);
+        results.forEach((palette) => {
+          const $palette = $(paletteHTML);
 
-                    //TODO: Update palette name, and author
-                    let $image = $palette.find('.palette-image');
-                    $image.attr('src', palette.imageUrl);
+          const $paletteImage = $palette.find('.palette-image');
+          const $paletteName = $palette.find('.palette-name');
+          const $paletteAuthor = $palette.find('.palette-author');
 
-                    let $name = $palette.find('.palette-name');
-                    $name.text(palette.title);
+          $paletteImage.attr('src', palette.imageUrl);
+          $paletteName.text(palette.title);
+          $paletteAuthor.text(palette.userName);
 
-                    let $author = $palette.find('.palette-author');
-                    $author.text(palette.userName);
-
-                    output.append($palette);
-                })
-                    .catch((err) => {
-                        console.error(err);
-                    });
-            });
-    });
+          output.append($palette);
+        });
+      })
+      .catch((err) => {
+        /* eslint-disable */
+        alert('shit hit the fan');
+        console.error('failed: ', err);
+        /* eslint-enable */
+      });
+  });
 });
+
 },{"./services/color-service":2}],2:[function(require,module,exports){
-module.exports = {
-    getColors: getColors,
-    searchColors: searchColors
-};
-
-const colourLoversAPI = 'http://www.colourlovers.com/api/colors';
-
-function getColors() {
-
-}
+const colourLoversAPI = '//www.colourlovers.com/api/colors';
 
 function searchColors(query, filters = {}) {
-    let queryParams = [];
-    for(let key in filters){
-        queryParams.push(`${key}=${filters[key]}`);
-    }
-    queryParams.push(`keywords=${query}`);
-    queryParams.push('jsonCallback=?');
+  const queryParams = [];
 
-    let searchUrl = `${colourLoversAPI}?${queryParams.join(`&`)}`;
+  /* eslint-disable-next-line */
+  for (const key in Object.keys(filters)) {
+    queryParams.push(`${key}=${filters[key]}`);
+  }
+  queryParams.push(`keywords=${query}`);
+  queryParams.push('jsonCallback=?');
 
+  const searchUrl = `${colourLoversAPI}?${queryParams.join('&')}`;
 
-    return new Promise((resolve, reject) => {
-        $.getJSON(searchUrl, resolve)
-            .fail((jqxhr, textStatus, error) => {
-                reject(error);
-            });
-    })
-
+  return new Promise((resolve, reject) => {
+    $.getJSON(searchUrl, resolve)
+      .fail((jqxhr, textStatus, error) => {
+        reject(error);
+      });
+  });
 }
+
+function getNewColors() {
+  // TODO: Write this
+}
+
+module.exports = {
+  searchColors,
+  getNewColors
+};
+
 },{}]},{},[1]);
